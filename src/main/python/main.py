27c2b89db5d7c7793afce9315292824c1c9b1e25
ND <njdurrah@berkeley.edu>
2020-04-11 18:38:55 -0400

    first commit

---
+++ src/main/python/main.py
@@ -0,0 +1,15 @@
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QLabel
+
+import sys
+
+if __name__ == '__main__':
+    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+    window = QMainWindow()
+    window.resize(250, 150)
+    window.show()
+    label = QLabel('Hello World!')
+    label.show()
+    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    sys.exit(exit_code)
