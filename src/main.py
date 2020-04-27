daa50f3e04ea17ae57b59d17d8e0a2f9cd42fdc6
ND <njdurrah@berkeley.edu>
2020-04-27 12:35:44 -0400

    added table

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
@@ -7,7 +8,13 @@ def window():
     window = QtWidgets.QWidget()
     window.setWindowTitle("Game")
     window.setGeometry(50, 50, 500, 500)
+    tableWidget = QtWidgets.QTableWidget(4, 4)
+    tableWidget.resizeColumnsToContents()
+    tableWidget.resize(500, 300)
+    tableWidget.show()
+
     window.show()
+
     sys.exc_info(app.exec_())
 
 window()
