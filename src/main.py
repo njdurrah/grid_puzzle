361793e90bb90eb3e0cfe7181bd57ba12414dde3
ND <njdurrah@berkeley.edu>
2020-04-30 18:23:17 -0400

    updated table

--- src/main.py
+++ src/main.py
@@ -5,15 +5,16 @@ from PyQt5 import QtCore
 def window():
     app = QtWidgets.QApplication(sys.argv)
 
-    window = QtWidgets.QWidget()
-    window.setWindowTitle("Game")
-    window.setGeometry(50, 50, 500, 500)
-    tableWidget = QtWidgets.QTableWidget(4, 4)
+
+    #window = QtWidgets.QWidget()
+    #window.setWindowTitle("Game")
+    #window.setGeometry(50, 50, 500, 500)
+    tableWidget = QtWidgets.QTableWidget(9, 9)
     tableWidget.resizeColumnsToContents()
-    tableWidget.resize(500, 300)
+    tableWidget.resize(500, 500)
     tableWidget.show()
 
-    window.show()
+    #window.show()
 
     sys.exc_info(app.exec_())
 
